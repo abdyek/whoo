2d7fa239cf90e8bf48d60b8f00cc5aec17b4448c
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2021-09-19 20:18:31 +0300

    DBNAME changed

--- src/Config/Propel.php
+++ src/Config/Propel.php
@@ -7,7 +7,7 @@ class Propel {
     public static $ADAPTER = 'mysql';
     public static $DSN = 'mysql:host=127.0.0.1;dbname=whoo';
     public static $HOST = '127.0.0.1';
-    public static $DBNAME = '127.0.0.1';
+    public static $DBNAME = 'whoo';
     public static $USER = 'root';
     public static $PASSWORD = '';
     public static $UTF8 = true;
