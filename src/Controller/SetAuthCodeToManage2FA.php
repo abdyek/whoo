89a0a34768b33863991e9f1c21befe9a23dd4ae4
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2021-12-29 20:44:45 +0300

    SetAuthCodeToManage2FA Controller and its test class updated with new core classes

--- src/Controller/SetAuthCodeToManage2FA.php
+++ src/Controller/SetAuthCodeToManage2FA.php
@@ -1,21 +1,30 @@
 <?php
 
 namespace Abdyek\Whoo\Controller;
-use Abdyek\Whoo\Core\Controller;
+use Abdyek\Whoo\Core\AbstractController;
 use Abdyek\Whoo\Model\AuthenticationCode;
 use Abdyek\Whoo\Model\User;
 use Abdyek\Whoo\Exception\IncorrectPasswordException;
 use Abdyek\Whoo\Config\Authentication as AuthConfig;
 use Abdyek\Whoo\Tool\Random;
 
-class SetAuthCodeToManage2FA extends Controller {
-    public $authCode;
-    protected function run() {
-        if(!User::checkPassword($this->user, $this->data['password'])) {
+class SetAuthCodeToManage2FA extends AbstractController
+{
+    public function run(): void
+    {
+        $content = $this->data->getContent();
+
+        $user = $this->authenticator->getUser();
+        if(!User::checkPassword($user, $content['password'])) {
             throw new IncorrectPasswordException;
         }
-        AuthenticationCode::deleteByUserIdType($this->user->getId(), AuthConfig::TYPE_MANAGE_2FA);
-        $this->authCode = Random::number(AuthConfig::$SIZE_OF_CODE_TO_MANAGE_2FA);
-        AuthenticationCode::create($this->user->getId(), AuthConfig::TYPE_MANAGE_2FA, $this->authCode);
+
+        AuthenticationCode::deleteByUserIdType($user->getId(), AuthConfig::TYPE_MANAGE_2FA);
+        $authCode = Random::number(AuthConfig::$SIZE_OF_CODE_TO_MANAGE_2FA);
+        AuthenticationCode::create($user->getId(), AuthConfig::TYPE_MANAGE_2FA, $authCode);
+
+        $this->response->setContent([
+            'authCode' => $authCode,
+        ]);
     }
 }
--- tests/unit/Controller/SetAuthCodeToManage2FATest.php
+++ tests/unit/Controller/SetAuthCodeToManage2FATest.php
@@ -4,55 +4,73 @@ use PHPUnit\Framework\TestCase;
 use Abdyek\Whoo\Controller\SetAuthCodeToManage2FA;
 use Abdyek\Whoo\Controller\SignUp;
 use Abdyek\Whoo\Controller\SignIn;
-use Abdyek\Whoo\Model\AuthenticationCode;
+use Abdyek\Whoo\Core\Config;
+use Abdyek\Whoo\Core\Data;
 use Abdyek\Whoo\Config\Authentication as AuthConfig;
 use Abdyek\Whoo\Exception\IncorrectPasswordException;
-use Abdyek\Whoo\Config\Whoo as Config;
-use Abdyek\Whoo\Config\Propel as PropelConfig;
+use Abdyek\Whoo\Model\AuthenticationCode;
+use Abdyek\Whoo\Model\User;
 
 /**
  * @covers SetAuthCodeToManage2FA::
  */
 
-class SetAuthCodeToManage2FATest extends TestCase {
-    use DefaultConfig;
+class SetAuthCodeToManage2FATest extends TestCase
+{
     use Reset;
-    public static function setUpBeforeClass(): void {
-        PropelConfig::$CONFIG_FILE = 'propel/config.php';
-    }
-    public function setUp(): void {
-        self::setDefaultConfig();
+
+    public function setUp(): void
+    {
         self::reset();
     }
-    public function testRun() {
-        $data = $this->getData();
-        Config::$USE_USERNAME = false;
-        Config::$DENY_IF_NOT_VERIFIED_TO_SIGN_IN = false;
-        Config::$DEFAULT_2FA = false;
-        new SignUp($data);
-        $signIn = new SignIn($data);
-        new SetAuthCodeToManage2FA([
-            'jwt'=>$signIn->jwt,
-            'password'=>$data['password']
-        ]);
-        $authCode = AuthenticationCode::getByUserIdType($signIn->user->getId(), AuthConfig::TYPE_MANAGE_2FA);
-        $this->assertNotNull($authCode->getCode());
-        $this->assertEquals(AuthConfig::$SIZE_OF_CODE_TO_MANAGE_2FA, strlen($authCode->getCode()));
+
+    public function testRun()
+    {
+        $content = $this->getContent();
+
+        $config = new Config();
+        $config->setUseUsername(false);
+        $config->setDenyIfNotVerifiedToSignIn(false);
+        $config->setDefault2fa(false);
+
+        (new SignUp(new Data($content), $config))->triggerRun();
+
+        ($signIn = new SignIn(new Data($content), $config))->triggerRun();
+
+        ($setAuth = new SetAuthCodeToManage2FA(new Data([
+            'jwt' => $signIn->getResponse()->getContent()['jwt'],
+            'password' => $content['password'],
+        ]), $config))->triggerRun();
+
+        $user = User::getByEmail($content['email']);
+
+        $authCode = AuthenticationCode::getByUserIdType($user->getId(), AuthConfig::TYPE_MANAGE_2FA);
+    
+        $this->assertSame($authCode->getCode(), $setAuth->getResponse()->getContent()['authCode']);
     }
-    public function testRunIncorrectPasswordException() {
+
+    public function testRunIncorrectPasswordException()
+    {
         $this->expectException(IncorrectPasswordException::class);
-        $data = $this->getData();
-        Config::$USE_USERNAME = false;
-        Config::$DENY_IF_NOT_VERIFIED_TO_SIGN_IN = false;
-        Config::$DEFAULT_2FA = false;
-        new SignUp($data);
-        $signIn = new SignIn($data);
-        new SetAuthCodeToManage2FA([
-            'jwt'=>$signIn->jwt,
-            'password'=>'wrong-password'
-        ]);
+        $content = $this->getContent();
+
+        $config = new Config();
+        $config->setUseUsername(false);
+        $config->setDenyIfNotVerifiedToSignIn(false);
+        $config->setDefault2fa(false);
+
+        (new SignUp(new Data($content), $config))->triggerRun();
+
+        ($signIn = new SignIn(new Data($content), $config))->triggerRun();
+
+        ($setAuth = new SetAuthCodeToManage2FA(new Data([
+            'jwt' => $signIn->getResponse()->getContent()['jwt'],
+            'password' => 'wrong-' . $content['password'],
+        ]), $config))->triggerRun();
     }
-    private function getData() {
+
+    private function getContent(): array
+    {
         return [
             'email'=>'this_is_email@email.com',
             'password'=>'this-is-password'
